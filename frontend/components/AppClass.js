d7493ba4f843219490d5c7f2abc0b02adfd17dea
Dionne Stratton <mylife33@hotmail.com>
2022-04-18 15:17:31 -0500

    mvp

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,40 +1,264 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+
+const initialState = {
+  count: 0,
+  vertical: 2,
+  horizontal: 2,
+  moveError: "",
+  email: "",
+};
+
+const URL = "http://localhost:9000/api/result";
 
 export default class AppClass extends React.Component {
+  state = initialState;
+  verticalControl = (key, value) => {
+    key === "up" && value > 1
+      ? this.setState({
+          ...this.state,
+          vertical: value - 1,
+          count: this.state.count + 1,
+          moveError: "",
+        })
+      : key === "up" && value === 1
+      ? this.setState({
+          ...this.state,
+          moveError: "You can't go up",
+        })
+      : key === "down" && value < 3
+      ? this.setState({
+          ...this.state,
+          vertical: value + 1,
+          count: this.state.count + 1,
+          moveError: "",
+        })
+      : this.setState({
+          ...this.state,
+          moveError: "You can't go down",
+        });
+  };
+  horizontalControl = (key, value) => {
+    key === "left" && value > 1
+      ? this.setState({
+          ...this.state,
+          horizontal: value - 1,
+          count: this.state.count + 1,
+          moveError: "",
+        })
+      : key === "left" && value === 1
+      ? this.setState({
+          ...this.state,
+          moveError: "You can't go left",
+        })
+      : key === "right" && value < 3
+      ? this.setState({
+          ...this.state,
+          horizontal: value + 1,
+          count: this.state.count + 1,
+          moveError: "",
+        })
+      : this.setState({
+          ...this.state,
+          moveError: "You can't go right",
+        });
+  };
+
+  resetHandler = () => {
+    this.setState({
+      ...this.state,
+      moveError: "",
+      email: "",
+      horizontal: 2,
+      vertical: 2,
+      count: 0,
+    });
+  };
+  onChange = (e) => {
+    this.setState({ email: e.target.value });
+  };
+
+  postToAxios = (e) => {
+    e.preventDefault();
+    const newSubs = {
+      x: this.state.horizontal,
+      y: this.state.vertical,
+      steps: this.state.count,
+      email: this.state.email,
+    };
+    axios
+      .post(URL, newSubs)
+      .then((res) => {
+        this.setState({ moveError: res.data.message });
+      })
+      .catch((err) => this.setState({ moveError: err.response.data.message }));
+    this.setState({
+      ...this.state,
+      email: "",
+    });
+  };
+
   render() {
-    const { className } = this.props
+    const { className } = this.props;
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">
+            Coordinates ({this.state.horizontal}, {this.state.vertical})
+          </h3>
+          <h3 id="steps">
+            You moved {this.state.count}{" "}
+            {this.state.count === 1 ? "time" : "times"}
+          </h3>
         </div>
         <div id="grid">
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square active">B</div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
+          <div
+            className={
+              this.state.horizontal === 1 && this.state.vertical === 1
+                ? "square active"
+                : "square"
+            }
+          >
+            {(this.state.horizontal === 1) & (this.state.vertical === 1)
+              ? "B"
+              : ""}
+          </div>
+          <div
+            className={
+              this.state.horizontal === 2 && this.state.vertical === 1
+                ? "square active"
+                : "square"
+            }
+          >
+            {(this.state.horizontal === 2) & (this.state.vertical === 1)
+              ? "B"
+              : ""}
+          </div>
+          <div
+            className={
+              this.state.horizontal === 3 && this.state.vertical === 1
+                ? "square active"
+                : "square"
+            }
+          >
+            {(this.state.horizontal === 3) & (this.state.vertical === 1)
+              ? "B"
+              : ""}
+          </div>
+          <div
+            className={
+              this.state.horizontal === 1 && this.state.vertical === 2
+                ? "square active"
+                : "square"
+            }
+          >
+            {(this.state.horizontal === 1) & (this.state.vertical === 2)
+              ? "B"
+              : ""}
+          </div>
+          <div
+            className={
+              this.state.horizontal === 2 && this.state.vertical === 2
+                ? "square active"
+                : "square"
+            }
+          >
+            {(this.state.horizontal === 2) & (this.state.vertical === 2)
+              ? "B"
+              : ""}
+          </div>
+          <div
+            className={
+              this.state.horizontal === 3 && this.state.vertical === 2
+                ? "square active"
+                : "square"
+            }
+          >
+            {(this.state.horizontal === 3) & (this.state.vertical === 2)
+              ? "B"
+              : ""}
+          </div>
+          <div
+            className={
+              this.state.horizontal === 1 && this.state.vertical === 3
+                ? "square active"
+                : "square"
+            }
+          >
+            {(this.state.horizontal === 1) & (this.state.vertical === 3)
+              ? "B"
+              : ""}
+          </div>
+          <div
+            className={
+              this.state.horizontal === 2 && this.state.vertical === 3
+                ? "square active"
+                : "square"
+            }
+          >
+            {(this.state.horizontal === 2) & (this.state.vertical === 3)
+              ? "B"
+              : ""}
+          </div>
+          <div
+            className={
+              this.state.horizontal === 3 && this.state.vertical === 3
+                ? "square active"
+                : "square"
+            }
+          >
+            {(this.state.horizontal === 3) & (this.state.vertical === 3)
+              ? "B"
+              : ""}
+          </div>
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{this.state.moveError}</h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button
+            id="left"
+            onClick={() =>
+              this.horizontalControl("left", this.state.horizontal)
+            }
+          >
+            LEFT
+          </button>
+          <button
+            id="up"
+            onClick={() => this.verticalControl("up", this.state.vertical)}
+          >
+            UP
+          </button>
+          <button
+            id="right"
+            onClick={() =>
+              this.horizontalControl("right", this.state.horizontal)
+            }
+          >
+            RIGHT
+          </button>
+          <button
+            id="down"
+            onClick={() => this.verticalControl("down", this.state.vertical)}
+          >
+            DOWN
+          </button>
+          <button id="reset" onClick={() => this.resetHandler()}>
+            reset
+          </button>
         </div>
         <form>
-          <input id="email" type="email" placeholder="type email"></input>
-          <input id="submit" type="submit"></input>
+          <input
+            id="email"
+            type="email"
+            placeholder="type email"
+            onChange={this.onChange}
+            value={this.state.email}
+          ></input>
+          <input id="submit" onClick={this.postToAxios} type="submit"></input>
         </form>
       </div>
-    )
+    );
   }
 }
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,37 +1,180 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function AppFunctional(props) {
+  const [count, setCount] = useState(0);
+  const [vertical, setVertical] = useState(2);
+  const [horizontal, setHorizontal] = useState(2);
+  const [moveError, setMoveError] = useState("");
+  const [email, setEmail] = useState("");
+
+  const goUp = () => {
+    vertical > 1
+      ? (setVertical((prevCount) => prevCount - 1),
+        setCount((prevCount) => prevCount + 1),
+        setMoveError(""))
+      : setMoveError("You can't go up");
+  };
+
+  const goDown = () => {
+    vertical < 3
+      ? (setVertical((prevCount) => prevCount + 1),
+        setCount((prevCount) => prevCount + 1),
+        setMoveError(""))
+      : setMoveError("You can't go down");
+  };
+
+  const goLeft = () => {
+    horizontal > 1
+      ? (setHorizontal((prevCount) => prevCount - 1),
+        setCount((prevCount) => prevCount + 1),
+        setMoveError(""))
+      : setMoveError("You can't go left");
+  };
+
+  const goRight = () => {
+    horizontal < 3
+      ? (setHorizontal((prevCount) => prevCount + 1),
+        setCount((prevCount) => prevCount + 1),
+        setMoveError(""))
+      : setMoveError("You can't go right");
+  };
+
+  const resetHandler = () => {
+    setCount(0);
+    setVertical(2);
+    setHorizontal(2);
+    setMoveError("");
+    setEmail("");
+  };
+
+  const postNewSubmission = (newSubmission) => {
+    axios
+      .post("http://localhost:9000/api/result", newSubmission)
+      .then((res) => {
+        // console.log('Error:')
+        // console.log(res.data)
+        setMoveError(res.data.message);
+      })
+      .catch((err) => setMoveError(err.response.data.message));
+  };
+
+  const formSubmit = (e) => {
+    const newSubmission = {
+      x: horizontal,
+      y: vertical,
+      steps: count,
+      email: email.trim(),
+    };
+    postNewSubmission(newSubmission);
+    e.preventDefault();
+    setEmail("");
+  };
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="coordinates">
+          Coordinates ({horizontal}, {vertical})
+        </h3>
+        <h3 id="steps">
+          You moved {count} {count === 1 ? "time" : "times"}
+        </h3>
       </div>
       <div id="grid">
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square active">B</div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
+        <div
+          className={
+            horizontal === 1 && vertical === 1 ? "square active" : "square"
+          }
+        >
+          {(horizontal === 1) & (vertical === 1) ? "B" : ""}
+        </div>
+        <div
+          className={
+            horizontal === 2 && vertical === 1 ? "square active" : "square"
+          }
+        >
+          {(horizontal === 2) & (vertical === 1) ? "B" : ""}
+        </div>
+        <div
+          className={
+            horizontal === 3 && vertical === 1 ? "square active" : "square"
+          }
+        >
+          {(horizontal === 3) & (vertical === 1) ? "B" : ""}
+        </div>
+        <div
+          className={
+            horizontal === 1 && vertical === 2 ? "square active" : "square"
+          }
+        >
+          {(horizontal === 1) & (vertical === 2) ? "B" : ""}
+        </div>
+        <div
+          className={
+            horizontal === 2 && vertical === 2 ? "square active" : "square"
+          }
+        >
+          {(horizontal === 2) & (vertical === 2) ? "B" : ""}
+        </div>
+        <div
+          className={
+            horizontal === 3 && vertical === 2 ? "square active" : "square"
+          }
+        >
+          {(horizontal === 3) & (vertical === 2) ? "B" : ""}
+        </div>
+        <div
+          className={
+            horizontal === 1 && vertical === 3 ? "square active" : "square"
+          }
+        >
+          {(horizontal === 1) & (vertical === 3) ? "B" : ""}
+        </div>
+        <div
+          className={
+            horizontal === 2 && vertical === 3 ? "square active" : "square"
+          }
+        >
+          {(horizontal === 2) & (vertical === 3) ? "B" : ""}
+        </div>
+        <div
+          className={
+            horizontal === 3 && vertical === 3 ? "square active" : "square"
+          }
+        >
+          {(horizontal === 3) & (vertical === 3) ? "B" : ""}
+        </div>
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{moveError}</h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button onClick={goLeft} id="left">
+          LEFT
+        </button>
+        <button onClick={goUp} id="up">
+          UP
+        </button>
+        <button onClick={goRight} id="right">
+          RIGHT
+        </button>
+        <button onClick={goDown} id="down">
+          DOWN
+        </button>
+        <button onClick={resetHandler} id="reset">
+          reset
+        </button>
       </div>
       <form>
-        <input id="email" type="email" placeholder="type email"></input>
-        <input id="submit" type="submit"></input>
+        <input
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="type email"
+        ></input>
+        <input id="submit" onClick={formSubmit} type="submit"></input>
       </form>
     </div>
-  )
+  );
 }
